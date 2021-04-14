066cf545ad15454fea44d8bbf1eab39b0ea11a84
mathg15 <69726961+mathg15@users.noreply.github.com>
2021-04-14 14:28:43 +0200

    Update Moosh.py

--- Moosh.py
+++ Moosh.py
@@ -31,14 +31,13 @@ class Bragg:
         # Définition de la polarisation
         self.pol = 1
 
-        # Nombre complexe
-        self.i = complex(0, 1)
+
 
     def cascade(self, A, B):
         # On combine deux matrices de diffusion A et B en une seule matrice de diffusion (2*2) S
-        t = 1 / (1 - B[1, 1] * A[2, 2])
-        S = np.array([[A[1, 1] + A[1, 2] * B[1, 1] * A[2, 1] * t, A[1, 2] * B[1, 2] * t],
-                      [B[2, 1] * A[2, 1] * t, B[2, 2] + A[2, 2] * B[1, 2] * B[2, 1] * t]])
+        t = 1 / (1 - B[0, 1] * A[1, 1])
+        S = np.array([[A[0, 0] + A[0, 1] * B[0, 0] * A[1, 0] * t, A[0, 1] * B[0, 1] * t],
+                      [B[1, 0] * A[1, 0] * t, B[1, 1] + A[1, 1] * B[0, 1] * B[1, 0] * t]])
         return S
 
     def coefficient(self):
@@ -81,41 +80,67 @@ class Bragg:
         # Condition de l'onde sortante pour le dernier milieu
         if np.real(TypeEps[g - 1]) < 0 and np.real(TypeMu[g - 1]) < 0 and np.real(
                 np.sqrt(TypeEps[g - 1] * TypeMu * (k0 ** 2) - (alpha ** 2))):
-            gamma[g] = -np.sqrt(TypeEps[g - 1] * TypeMu[g - 1] - (alpha ** 2))
+            gamma[g - 1] = -np.sqrt(TypeEps[g - 1] * TypeMu[g - 1] - (alpha ** 2))
         else:
             gamma[g - 1] = np.sqrt(TypeEps[g - 1] * TypeMu[g - 1] * (k0 ** 2) - (alpha ** 2))
 
         # Définition de la matrice T
-        T = np.array([[[0, 1], [1, 0]], [[0, 0], [0, 0]], [[0, 0], [0, 0]]])
-        # Cacul des matrices S
-        for k in range(0, g - 2, 1):
-            # Matrice de diffusion des couches
-            t = np.exp(self.i * gamma[k] * self.height[k])
-            T[2 * k, :, :] = np.array([[0, t], [t, 0]])
+        # T = np.array([[[0, 1], [1, 0]], [[0, 0], [0, 0]], [[0, 0], [0, 0]]])
+        # # Cacul des matrices S
+        # for k in range(0, g - 2, 1):
+        #     # Matrice de diffusion des couches
+        #     t = np.exp(1j * gamma[k] * self.height[k])
+        #     T[2 * k, :, :] = np.array([[0, t], [t, 0]])
+        #
+        #     # Matrice de diffusion d'interface
+        #     b1 = gamma[k] / (f[k])
+        #     b2 = gamma[k + 1] / (f[k + 1])
+        #     T[2 * k + 1, :, :] = np.array(
+        #         [[(b1 - b2) / (b1 + b2), (2 * b2) / (b1 + b2)], [(2 * b1) / (b1 + b2), (b2 - b1) / (b1 + b2)]])
+        #
+        # # Matrice de diffusion pour la dernière couche
+        # t = np.exp(1j * gamma[g - 1] * self.height[g - 1])
+        # T[2 * g, :, :] = np.array([[0, t], [t, 0]])
 
-            # Matrice de diffusion d'interface
+        a = np.array([[[1, 0], [0, 1]]])
+
+        for k in range(0, g-1, 1):
+            t = np.exp(1j * gamma[k] * self.height[k])
+            B = np.array([[[t, 0], [0, t]]])
+
             b1 = gamma[k] / (f[k])
             b2 = gamma[k + 1] / (f[k + 1])
-            T[2 * k + 1, :, :] = np.array(
-                [[(b1 - b2) / (b1 + b2), (2 * b2) / (b1 + b2)], [(2 * b1) / (b1 + b2), (b2 - b1) / (b1 + b2)]])
+            # Ajouter des crochets
+            C = np.array(
+                [[[(b1 - b2) / (b1 + b2), (2 * b2) / (b1 + b2)], [(2 * b1) / (b1 + b2), (b2 - b1) / (b1 + b2)]]])
+            if k % 2 == 0:
+                a = np.concatenate((a, B), axis=0)
+            else:
+                a = np.concatenate((a, C), axis=0)
 
-        # Matrice de diffusion pour la dernière couche
-        t = np.exp(self.i * gamma[g - 1] * self.height[g - 1])
-        T[2 * g, :, :] = np.array([[0, t], [t, 0]])
+        D = np.array([[[t, 0], [0, t]]])
+        a = np.concatenate((a, D), axis=0)
+
+        print(a)
 
         # On combine les différentes matrices
-        A[0, :, :] = T[0, :, :]
-        for i in range(0, T.shape[1] - 2, 1):
-            A[i + 1] = self.cascade(A[i, :, :], T[i + 1, :, :])
+        # A = np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]], [[0, 0], [0, 0]]])
+        # A[0, :, :] = T[0, :, :]
+        # for i in range(0, T.shape[1] - 2, 1):
+        #     A[i + 1] = self.cascade(A[i, :, :], T[i + 1, :, :])
 
         # Coefficient de reflexion de l'ensemble de la structure
-        r = A[A.shape[1], :, :][0, 0]
+        r = a[a.shape[1], :, :][0, 0]
+        print(r)
         # Coefficient de transmission de l'ensemble de la structure
-        tr = A[A.shape[1], :, :][1, 0]
+        tr = a[a.shape[1], :, :][1, 0]
+        print(tr)
         # Coefficient de réflexion de l'énergie
         R = np.abs(r) ** 2
+        print(R)
         # Coefficient de transmission de l'énergie
-        Tr = (np.abs(t) ** 2) * gamma[g - 1] * f[0] / (gamma[1] * f[g])
+        Tr = (np.abs(t) ** 2) * gamma[g - 1] * f[0] / (gamma[0] * f[g - 1])
+        print(Tr)
 
         return r, tr, R, Tr
 
@@ -124,5 +149,6 @@ theta = (35 * np.pi) / 180
 
 a = Bragg(20, 600, theta)
 a.coefficient()
+
 
 
