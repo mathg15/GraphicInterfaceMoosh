14966bfa7a86d1b27f650120465c7605ce7cae1b
mathg15 <69726961+mathg15@users.noreply.github.com>
2021-04-15 18:33:41 +0200

    Update Moosh.py

--- Moosh.py
+++ Moosh.py
@@ -1,9 +1,10 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class Bragg:
 
-    def __init__(self, periods, Lambda, Theta):
+    def __init__(self, periods, Lambda, Theta, Npoints):
 
         # On prend des valeurs de Lambda et Theta
         self.Lambda = Lambda
@@ -15,6 +16,10 @@ class Bragg:
 
         # On définie le nombre de période
         self.n_periods = periods
+
+        # On définie le nombre de points
+
+        self.Npoints = Npoints
 
         # Définition de la structure du matériau => ( 0, 1, 2, 1, 2, ...., 1, 2, 0)
         rep0 = np.tile([1, 2], (1, self.n_periods))
@@ -31,8 +36,6 @@ class Bragg:
         # Définition de la polarisation
         self.pol = 1
 
-
-
     def cascade(self, A, B):
         # On combine deux matrices de diffusion A et B en une seule matrice de diffusion (2*2) S
         t = 1 / (1 - B[0, 1] * A[1, 1])
@@ -40,7 +43,7 @@ class Bragg:
                       [B[1, 0] * A[1, 0] * t, B[1, 1] + A[1, 1] * B[0, 1] * B[1, 0] * t]])
         return S
 
-    def coefficient(self):
+    def coefficient(self, a):
 
         # On considère que la première valeur de la hauteur est 0
         self.height[0] = 0
@@ -67,8 +70,9 @@ class Bragg:
             f = TypeEps
 
         # Définition de alpha et gamma en fonction de TypeEps, TypeMu, k0 et Theta
-        alpha = np.sqrt(TypeEps[0] * TypeMu[0]) * k0 * np.sin(self.Theta)
+        alpha = np.sqrt(TypeEps[0] * TypeMu[0]) * k0 * np.sin(a * (np.pi / 180))
         gamma = np.sqrt(TypeEps * TypeMu * (k0 ** 2) - np.ones(g) * (alpha ** 2))
+        # print("gamma=",gamma)
 
         # On fait en sorte d'avoir un résultat positif en foction au cas où l'index serait négatif
         if np.real(TypeEps[0]) < 0 and np.real(TypeMu[0]):
@@ -104,51 +108,84 @@ class Bragg:
 
         a = np.array([[[1, 0], [0, 1]]])
 
-        for k in range(0, g-1, 1):
+        for k in range(g - 3):
+            z = gamma[k]
+            # print("z=",z)
             t = np.exp(1j * gamma[k] * self.height[k])
             B = np.array([[[t, 0], [0, t]]])
 
             b1 = gamma[k] / (f[k])
+            # print("b1", b1, k)
             b2 = gamma[k + 1] / (f[k + 1])
-            # Ajouter des crochets
+            # print("b2", b2, k)
+
             C = np.array(
                 [[[(b1 - b2) / (b1 + b2), (2 * b2) / (b1 + b2)], [(2 * b1) / (b1 + b2), (b2 - b1) / (b1 + b2)]]])
             if k % 2 == 0:
                 a = np.concatenate((a, B), axis=0)
             else:
                 a = np.concatenate((a, C), axis=0)
-
+        t = np.exp(1j * gamma[g - 1] * self.height[g - 1])
         D = np.array([[[t, 0], [0, t]]])
         a = np.concatenate((a, D), axis=0)
 
-        print(a)
+        # print("avant", a)
 
         # On combine les différentes matrices
-        # A = np.array([[[0, 0], [0, 0]], [[0, 0], [0, 0]], [[0, 0], [0, 0]]])
-        # A[0, :, :] = T[0, :, :]
-        # for i in range(0, T.shape[1] - 2, 1):
-        #     A[i + 1] = self.cascade(A[i, :, :], T[i + 1, :, :])
+        a_ = np.copy(a)
+
+        for i in range(0, a.shape[1] - 2, 1):
+            a[i + 1] = self.cascade(a[i], a_[i + 1])
+
+        # print("après", a)
 
         # Coefficient de reflexion de l'ensemble de la structure
-        r = a[a.shape[1], :, :][0, 0]
-        print(r)
+        r = a[a.shape[1]][0, 1]
+        # print("r=", r)
         # Coefficient de transmission de l'ensemble de la structure
-        tr = a[a.shape[1], :, :][1, 0]
-        print(tr)
+        tr = a[a.shape[1], :, :][1, 1]
+        # print("t=", tr)
         # Coefficient de réflexion de l'énergie
         R = np.abs(r) ** 2
-        print(R)
+        # print("R=", R)
         # Coefficient de transmission de l'énergie
-        Tr = (np.abs(t) ** 2) * gamma[g - 1] * f[0] / (gamma[0] * f[g - 1])
-        print(Tr)
+        Tr = (np.abs(tr) ** 2)
+        # * gamma[g - 1] * f[0] / (gamma[0] * f[g - 1])
+        # print("T=", Tr)
 
         return r, tr, R, Tr
+
+    # def affichageCoef(self):
+
+    def angular(self):
+
+        # Intervalle angulaire
+        angMax = 90
+        angMin = 0
+        Range_angl = np.linspace(angMin, angMax, self.Npoints)
+
+        # print(Range_angl)
+        a = np.ones((self.Npoints, 1))
+        b = np.ones((self.Npoints, 1))
+        c = np.ones((self.Npoints, 1))
+        d = np.ones((self.Npoints, 1))
+
+        for i in range(self.Npoints - 1):
+            a[i], b[i], c[i], d[i] = self.coefficient(i)
+
+        fig, ax = plt.subplots()  # Create a figure and an axes.
+        ax.plot(Range_angl, c)  # Plot some data on the axes.
+        ax.set_xlabel('Angle')  # Add an x-label to the axes.
+        ax.set_ylabel('Reflexion')  # Add a y-label to the axes.
+        ax.set_title("Reflection")  # Add a title to the axes.
+        ax.legend()
+        plt.show()
 
 
 theta = (35 * np.pi) / 180
 
-a = Bragg(20, 600, theta)
-a.coefficient()
-
+a = Bragg(20, 600, theta, 200)
+a.coefficient(35)
+a.angular()
 
 
