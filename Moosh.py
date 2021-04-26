d33f5bffc05448b7d1c604760550661318d91611
mathg15 <69726961+mathg15@users.noreply.github.com>
2021-04-27 00:24:34 +0200

    Update Moosh.py

--- Moosh.py
+++ Moosh.py
@@ -4,11 +4,7 @@ import matplotlib.pyplot as plt
 
 class Bragg:
 
-    def __init__(self, periods, Lambda, Theta, Npoints):
-
-        # On prend des valeurs de Lambda et Theta
-        self.Lambda = Lambda
-        self.Theta = Theta
+    def __init__(self, periods, Npoints):
 
         # On définie la permittivité et la perméabilité
         self.Eps = np.array([1, 2, 4])
@@ -36,20 +32,23 @@ class Bragg:
         # Définition de la polarisation
         self.pol = 1
 
+        self.i = complex(0, 1)
+
     def cascade(self, A, B):
         # On combine deux matrices de diffusion A et B en une seule matrice de diffusion (2*2) S
-        t = 1 / (1 - B[0, 1] * A[1, 1])
+        t = 1 / (1 - B[0, 0] * A[1, 1])
         S = np.array([[A[0, 0] + A[0, 1] * B[0, 0] * A[1, 0] * t, A[0, 1] * B[0, 1] * t],
-                      [B[1, 0] * A[1, 0] * t, B[1, 1] + A[1, 1] * B[0, 1] * B[1, 0] * t]])
+                      [B[1, 0] * A[1, 0] * t, B[1, 1] + A[1, 1] * B[0, 1] * B[1, 0] * t]], dtype=complex)
+
         return S
 
-    def coefficient(self, a):
+    def coefficient(self, thetacoef, _lambda):
 
         # On considère que la première valeur de la hauteur est 0
         self.height[0] = 0
 
         # On definie k0 à partir de lambda
-        k0 = (2 * np.pi) / self.Lambda
+        k0 = (2 * np.pi) / _lambda
 
         # g est la longeur de Type
         g = len(self.Type)
@@ -70,7 +69,7 @@ class Bragg:
             f = TypeEps
 
         # Définition de alpha et gamma en fonction de TypeEps, TypeMu, k0 et Theta
-        alpha = np.sqrt(TypeEps[0] * TypeMu[0]) * k0 * np.sin(a * (np.pi / 180))
+        alpha = np.sqrt(TypeEps[0] * TypeMu[0]) * k0 * np.sin(thetacoef * (np.pi / 180))
         gamma = np.sqrt(TypeEps * TypeMu * (k0 ** 2) - np.ones(g) * (alpha ** 2))
         # print("gamma=",gamma)
 
@@ -89,103 +88,121 @@ class Bragg:
             gamma[g - 1] = np.sqrt(TypeEps[g - 1] * TypeMu[g - 1] * (k0 ** 2) - (alpha ** 2))
 
         # Définition de la matrice T
+        T = np.zeros((2 * g, 2, 2), dtype=complex)
+        T[0] = [[0, 1], [1, 0]]
         # T = np.array([[[0, 1], [1, 0]], [[0, 0], [0, 0]], [[0, 0], [0, 0]]])
-        # # Cacul des matrices S
-        # for k in range(0, g - 2, 1):
-        #     # Matrice de diffusion des couches
-        #     t = np.exp(1j * gamma[k] * self.height[k])
-        #     T[2 * k, :, :] = np.array([[0, t], [t, 0]])
-        #
-        #     # Matrice de diffusion d'interface
-        #     b1 = gamma[k] / (f[k])
-        #     b2 = gamma[k + 1] / (f[k + 1])
-        #     T[2 * k + 1, :, :] = np.array(
-        #         [[(b1 - b2) / (b1 + b2), (2 * b2) / (b1 + b2)], [(2 * b1) / (b1 + b2), (b2 - b1) / (b1 + b2)]])
-        #
-        # # Matrice de diffusion pour la dernière couche
-        # t = np.exp(1j * gamma[g - 1] * self.height[g - 1])
-        # T[2 * g, :, :] = np.array([[0, t], [t, 0]])
+        # Cacul des matrices S
+        for k in range(g - 1):
+            # Matrice de diffusion des couches
+            t = np.exp(self.i * gamma[k] * self.height[k])
+            T[2 * k + 1] = np.array([[0, t], [t, 0]])
 
-        a = np.array([[[1, 0], [0, 1]]])
-
-        for k in range(g - 3):
-            z = gamma[k]
-            # print("z=",z)
-            t = np.exp(1j * gamma[k] * self.height[k])
-            B = np.array([[[t, 0], [0, t]]])
-
+            # Matrice de diffusion d'interface
             b1 = gamma[k] / (f[k])
-            # print("b1", b1, k)
             b2 = gamma[k + 1] / (f[k + 1])
-            # print("b2", b2, k)
+            T[2 * k + 2] = [[(b1 - b2) / (b1 + b2), (2 * b2) / (b1 + b2)],
+                            [(2 * b1) / (b1 + b2), (b2 - b1) / (b1 + b2)]]
 
-            C = np.array(
-                [[[(b1 - b2) / (b1 + b2), (2 * b2) / (b1 + b2)], [(2 * b1) / (b1 + b2), (b2 - b1) / (b1 + b2)]]])
-            if k % 2 == 0:
-                a = np.concatenate((a, B), axis=0)
-            else:
-                a = np.concatenate((a, C), axis=0)
-        t = np.exp(1j * gamma[g - 1] * self.height[g - 1])
-        D = np.array([[[t, 0], [0, t]]])
-        a = np.concatenate((a, D), axis=0)
+        # Matrice de diffusion pour la dernière couche
+        t = np.exp(self.i * gamma[g - 1] * self.height[g - 1])
+        T[2 * g - 1] = [[0, t], [t, 0]]
 
-        # print("avant", a)
+        A = np.zeros((2 * g - 1, 2, 2), dtype=complex)
+        A[0] = T[0]
 
-        # On combine les différentes matrices
-        a_ = np.copy(a)
-
-        for i in range(0, a.shape[1] - 2, 1):
-            a[i + 1] = self.cascade(a[i], a_[i + 1])
-
-        # print("après", a)
+        for j in range(len(T) - 2):
+            A[j + 1] = self.cascade(A[j], T[j + 1])
 
         # Coefficient de reflexion de l'ensemble de la structure
-        r = a[a.shape[1]][0, 1]
-        # print("r=", r)
+        r = A[len(A) - 1][0, 0]
+
         # Coefficient de transmission de l'ensemble de la structure
-        tr = a[a.shape[1], :, :][1, 1]
-        # print("t=", tr)
+        tr = A[len(A) - 1][1, 0]
+
         # Coefficient de réflexion de l'énergie
         R = np.abs(r) ** 2
-        # print("R=", R)
+
         # Coefficient de transmission de l'énergie
-        Tr = (np.abs(tr) ** 2)
-        # * gamma[g - 1] * f[0] / (gamma[0] * f[g - 1])
-        # print("T=", Tr)
+        Tr = (np.abs(tr) ** 2) * gamma[g - 1] * f[0] / (gamma[0] * f[g - 1])
 
         return r, tr, R, Tr
 
-    # def affichageCoef(self):
+    def affichageCoef(self, angle, longueurOnde):
 
-    def angular(self):
+        reflexionT, transmissionT, reflexionE, transmissionE = self.coefficient(angle, longueurOnde)
+
+        print("Coefficient de reflexion de l'ensemble de la structure :", reflexionT)
+        print("Coefficient de transmission de l'ensemble de la structure :", transmissionT)
+        print("Coefficient de transmission de l'énergie :", reflexionE)
+        print("Coefficient de transmission de l'énergie :", transmissionE)
+
+    def angular(self, lambda_):
 
         # Intervalle angulaire
-        angMax = 90
-        angMin = 0
-        Range_angl = np.linspace(angMin, angMax, self.Npoints)
+        maxAngle = 89
+        minAngle = 0
+        rangeAngle = np.linspace(minAngle, maxAngle, self.Npoints)
 
-        # print(Range_angl)
-        a = np.ones((self.Npoints, 1))
-        b = np.ones((self.Npoints, 1))
-        c = np.ones((self.Npoints, 1))
-        d = np.ones((self.Npoints, 1))
+        # Création des matrices
+        a = np.ones((self.Npoints, 1), dtype=complex)
+        b = np.ones((self.Npoints, 1), dtype=complex)
+        c = np.ones((self.Npoints, 1), dtype=complex)
+        d = np.ones((self.Npoints, 1), dtype=complex)
 
-        for i in range(self.Npoints - 1):
-            a[i], b[i], c[i], d[i] = self.coefficient(i)
+        for i in range(self.Npoints):
+            tht = rangeAngle[i]
+            a[i], b[i], c[i], d[i] = self.coefficient(tht, lambda_)
 
-        fig, ax = plt.subplots()  # Create a figure and an axes.
-        ax.plot(Range_angl, c)  # Plot some data on the axes.
-        ax.set_xlabel('Angle')  # Add an x-label to the axes.
-        ax.set_ylabel('Reflexion')  # Add a y-label to the axes.
-        ax.set_title("Reflection")  # Add a title to the axes.
-        ax.legend()
+        plt.figure(1)
+        plt.subplot(211)
+        plt.title("Reflexion for lambda = 600 nm")
+        plt.plot(rangeAngle, abs(c))
+        plt.ylabel("Reflexion")
+        plt.xlabel("Angle (degrees)")
+        plt.subplot(212)
+        plt.plot(rangeAngle, np.angle(a))
+        plt.ylabel("Phase")
+        plt.xlabel("Angle")
+        plt.title("Phase of the reflection coefficient for lambda = 600 nm")
+        plt.tight_layout()
+        plt.show()
+
+    def spectrum(self, theta_):
+
+        # Intervalle de longeur d'onde
+        minLambda = 400
+        maxLambda = 700
+        rangeLambda = np.linspace(minLambda, maxLambda, self.Npoints)
+
+        a = np.ones((self.Npoints, 1), dtype=complex)
+        b = np.ones((self.Npoints, 1), dtype=complex)
+        c = np.ones((self.Npoints, 1), dtype=complex)
+        d = np.ones((self.Npoints, 1), dtype=complex)
+
+        for i in range(self.Npoints):
+            rL = rangeLambda[i]
+            a[i], b[i], c[i], d[i] = self.coefficient(theta_, rL)
+
+        plt.figure(1)
+        plt.subplot(211)
+        plt.title("Reflexion for lambda = 600 nm")
+        plt.plot(rangeLambda, abs(c))
+        plt.ylabel("Reflexion")
+        plt.xlabel("Angle (degrees)")
+        plt.subplot(212)
+        plt.plot(rangeLambda, np.angle(a))
+        plt.ylabel("Phase")
+        plt.xlabel("Angle")
+        plt.title("Phase of the reflection coefficient for lambda = 600 nm")
+        plt.tight_layout()
         plt.show()
 
 
 theta = (35 * np.pi) / 180
 
-a = Bragg(20, 600, theta, 200)
-a.coefficient(35)
-a.angular()
+a = Bragg(20, 200)
+
+a.angular(600)
+a.spectrum(theta)
 
 
