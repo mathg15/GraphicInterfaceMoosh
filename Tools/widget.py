53f49b1f6346bae1c1d3a9f3f94be4191dc1d654
mathg15 <69726961+mathg15@users.noreply.github.com>
2021-08-21 21:56:52 +0200

    Update widget.py

--- Tools/widget.py
+++ Tools/widget.py
@@ -10,25 +10,25 @@ class sidebarWidget:
         return st.radio("Polarisation", ("Polarisation TE", "Polarisation TM"))
 
     def angleInput1(self):
-        return st.number_input("Angle d'incidence", 1, 90, 35, format=None, key=1)
+        return st.number_input("Angle d'incidence", 1, 90, 35, format=None)
 
     def angleInput2(self):
-        return st.number_input("Angle d'incidence", 0, 90, 35, format=None, key=2)
+        return st.number_input("Angle d'incidence", 0, 90, 35, format=None)
 
     def angleInput3(self):
-        return st.number_input("Angle d'incidence", 0, 90, 35, format=None, key=3)
+        return st.number_input("Angle d'incidence", 0, 90, 35, format=None)
 
     def lambdaInput1(self):
-        return st.number_input("Longueur d'onde", 400, 800, 600, format=None, key=1)
+        return st.number_input("Longueur d'onde", 400, 800, 600, format=None)
 
     def lambdaInput2(self):
-        return st.number_input("Longueur d'onde", 400, 800, 600, format=None, key=2)
+        return st.number_input("Longueur d'onde", 400, 800, 600, format=None)
 
     def lambdaInput3(self):
-        return st.number_input("Longueur d'onde", 400, 800, 600, format=None, key=3)
+        return st.number_input("Longueur d'onde", 400, 800, 600, format=None)
 
     def lambdaInput4(self):
-        return st.number_input("Longueur d'onde", 400, 800, 600, format=None, key=4)
+        return st.number_input("Longueur d'onde", 400, 800, 600, format=None)
 
     def beamPos(self):
         return st.slider("Position", 0.0, 1.0, 0.4, 0.1)
@@ -43,76 +43,76 @@ class sidebarWidget:
         return st.selectbox("Choix du matériau 1", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"))
 
     def hauteurGen1(self):
-        return st.number_input("Epaisseur du matériau 1", 1, 20000, 400, 1, key=1)
+        return st.number_input("Epaisseur du matériau 1", 1, 20000, 400, 1)
 
     def selecBoxGen2(self):
-        return st.selectbox("Choix du matériau 2", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"), key=2)
+        return st.selectbox("Choix du matériau 2", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"))
 
     def hauteurGen2(self):
-        return st.number_input("Epaisseur du matériau 2", 1, 20000, 400, 1, key=2)
+        return st.number_input("Epaisseur du matériau 2", 1, 20000, 400, 1)
 
     def selecBoxGen3(self):
-        return st.selectbox("Choix du matériau 3", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"), key=3)
+        return st.selectbox("Choix du matériau 3", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"))
 
     def hauteurGen3(self):
-        return st.number_input("Epaisseur du matériau 3", 1, 20000, 400, 1, key=3)
+        return st.number_input("Epaisseur du matériau 3", 1, 20000, 400, 1)
 
     def selecBoxGen4(self):
-        return st.selectbox("Choix du matériau 4", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"), key=4)
+        return st.selectbox("Choix du matériau 4", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"))
 
     def hauteurGen4(self):
-        return st.number_input("Epaisseur du matériau 4", 1, 20000, 400, 1, key=4)
+        return st.number_input("Epaisseur du matériau 4", 1, 20000, 400, 1)
 
     def selecBoxGen5(self):
-        return st.selectbox("Choix du matériau 5", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"), key=5)
+        return st.selectbox("Choix du matériau 5", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"))
 
     def hauteurGen5(self):
-        return st.number_input("Epaisseur du matériau 5", 1, 20000, 400, 1, key=5)
+        return st.number_input("Epaisseur du matériau 5", 1, 20000, 400, 1)
 
     def selecBoxGen6(self):
-        return st.selectbox("Choix du matériau 6", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"), key=6)
+        return st.selectbox("Choix du matériau 6", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"))
 
     def hauteurGen6(self):
-        return st.number_input("Epaisseur du matériau 6", 1, 20000, 400, 1, key=6)
+        return st.number_input("Epaisseur du matériau 6", 1, 20000, 400, 1)
 
     def selecBoxGen7(self):
-        return st.selectbox("Choix du matériau 7", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"), key=7)
+        return st.selectbox("Choix du matériau 7", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"))
 
     def hauteurGen7(self):
-        return st.number_input("Epaisseur du matériau 7", 1, 20000, 400, 1, key=7)
+        return st.number_input("Epaisseur du matériau 7", 1, 20000, 400, 1)
 
     def selecBoxGen8(self):
-        return st.selectbox("Choix du matériau 8", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"), key=8)
+        return st.selectbox("Choix du matériau 8", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"))
 
     def hauteurGen8(self):
-        return st.number_input("Epaisseur du matériau 8", 1, 20000, 400, 1, key=8)
+        return st.number_input("Epaisseur du matériau 8", 1, 20000, 400, 1)
 
     def selecBoxGen9(self):
-        return st.selectbox("Choix du matériau 9", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"), key=9)
+        return st.selectbox("Choix du matériau 9", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"))
 
     def hauteurGen9(self):
-        return st.number_input("Epaisseur du matériau 9", 1, 20000, 400, 1, key=9)
+        return st.number_input("Epaisseur du matériau 9", 1, 20000, 400, 1)
 
     def selecBoxGen10(self):
-        return st.selectbox("Choix du matériau 10", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"), key=10)
+        return st.selectbox("Choix du matériau 10", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"))
 
     def hauteurGen10(self):
-        return st.number_input("Epaisseur du matériau 10", 1, 20000, 400, 1, key=10)
+        return st.number_input("Epaisseur du matériau 10", 1, 20000, 400, 1)
 
     def angleCoefGen(self):
-        return st.number_input("Angle d'incidence", 0, 90, 35, format=None, key=4)
+        return st.number_input("Angle d'incidence", 0, 90, 35, format=None)
 
     def lambdaCoefGen(self):
-        return st.number_input("Longueur d'onde", 400, 800, 600, format=None, key=5)
+        return st.number_input("Longueur d'onde", 400, 800, 600, format=None)
 
     def lambAngGen(self):
-        return st.number_input("Longueur d'onde", 400, 800, 600, format=None, key=6)
+        return st.number_input("Longueur d'onde", 400, 800, 600, format=None)
 
     def btnAngGen(self):
         return st.button("Afficher Angular")
 
     def thetaSpecGen(self):
-        return st.number_input("Angle d'incidence", 0, 90, 35, format=None, key=5)
+        return st.number_input("Angle d'incidence", 0, 90, 35, format=None)
 
     def btnSpecGen(self):
         return st.button("Afficher Spectrum")
@@ -121,7 +121,7 @@ class sidebarWidget:
         return st.button("Afficher les coefficients")
 
     def PermiSetGen(self):
-        return st.number_input("Longueur d'onde", 400, 800, 600, format=None, key=7)
+        return st.number_input("Longueur d'onde", 400, 800, 600, format=None)
 
     def multiSelectPolaGen(self):
         return st.selectbox("Polarisation", ["Polarisation TE", "Polarisation TM"])
@@ -130,19 +130,19 @@ class sidebarWidget:
         return st.radio("", ("Reflexion", "Transmission"))
 
     def minWinAngGen(self):
-        return st.number_input("Angle minimum", 0, 90, 0, format=None, key=7)
+        return st.number_input("Angle minimum", 0, 90, 0, format=None)
 
     def maxWinAngGen(self):
-        return st.number_input("Angle maximum", 0, 90, 90, format=None, key=8)
+        return st.number_input("Angle maximum", 0, 90, 90, format=None)
 
     def lambBeamGen(self):
-        return st.number_input("Longueur d'onde", 400, 800, 600, key=8)
+        return st.number_input("Longueur d'onde", 400, 800, 600)
 
     def angBeamGen(self):
-        return st.number_input("Angle", 1.0, 90.0, 45.0, 0.1, key=9)
+        return st.number_input("Angle", 1.0, 90.0, 45.0, 0.1)
 
     def posBeamGen(self):
-        return st.slider("Position", 0.0, 1.0, 0.4, 0.1, key=2)
+        return st.slider("Position", 0.0, 1.0, 0.4, 0.1)
 
     def btnBeamGen(self):
         return st.button("Afficher Beam")
@@ -151,22 +151,22 @@ class sidebarWidget:
         return st.selectbox("Polarisation", ["TE", "TM"])
 
     def angCoefSPR(self):
-        return st.number_input("Angle", 1.0, 90.0, 45.0, 0.1, key=10)
+        return st.number_input("Angle", 1.0, 90.0, 45.0, 0.1)
 
     def angSpecSPR(self):
-        return st.number_input("Angle", 1.0, 90.0, 45.0, 0.1, key=11)
+        return st.number_input("Angle", 1.0, 90.0, 45.0, 0.1)
 
     def angBeamSPR(self):
-        return st.number_input("Angle", 1.0, 90.0, 45.0, 0.1, key=12)
+        return st.number_input("Angle", 1.0, 90.0, 45.0, 0.1)
 
     def lambCoefSPR(self):
-        return st.number_input("Longueur d'onde", 400, 800, 600, key=9)
+        return st.number_input("Longueur d'onde", 400, 800, 600)
 
     def lambAngSPR(self):
-        return st.number_input("Longueur d'onde", 400, 800, 600, key=10)
+        return st.number_input("Longueur d'onde", 400, 800, 600)
 
     def lambBeamSPR(self):
-        return st.number_input("Longueur d'onde", 400, 800, 600, key=11)
+        return st.number_input("Longueur d'onde", 400, 800, 600)
 
     def btnAngSPR(self):
         return st.button("Afficher Angular")
@@ -181,10 +181,10 @@ class sidebarWidget:
         return st.button("Afficher Beam")
 
     def posBeamSPR(self):
-        return st.slider("Position", 0.0, 1.0, 0.4, 0.1, key=3)
+        return st.slider("Position", 0.0, 1.0, 0.4, 0.1)
 
     def absLambGen(self):
-        return st.number_input("Longueur d'onde", 400, 800, 600, key=12)
+        return st.number_input("Longueur d'onde", 400, 800, 600)
 
     def btnAbsGen(self):
         return st.button("Afficher l'absorption")
