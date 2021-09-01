76d35357a2f5082eb2f9dcc1b57f6e547500abc9
mathg15 <mathis.granger15@gmail.com>
2021-09-01 19:17:27 +0200

    Update homepage.py

--- Pages/homepage.py
+++ Pages/homepage.py
@@ -3,3 +3,7 @@ import streamlit as st
 def homepage():
     st.title("Moosh")
     st.header("Le couteau suisse numérique pour l'optique")
+    st.write("")
+    st.write("Le logiciel Moosh a pour objectif de modéliser un faisceau lumineux à travers un structure multicouche.")
+    st.header("Examples de réalisations : ")
+    st.image
