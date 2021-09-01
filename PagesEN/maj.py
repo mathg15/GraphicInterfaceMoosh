a427db7323e7bacfdf7d071d25e17e53031e8af2
mathg15 <mathis.granger15@gmail.com>
2021-09-01 18:15:39 +0200

    Update maj.py

--- PagesEN/maj.py
+++ PagesEN/maj.py
@@ -2,4 +2,7 @@ import streamlit as st
 
 
 def maj():
-    st.title('Updates')
+    st.title("Updates")
+    st.markdown("Update 1.0.1 (01/09/2021)")
+    st.write(" - Addition of the page Mises à jour / Update")
+    st.write(" - Added language selection (FR/EN) ")
