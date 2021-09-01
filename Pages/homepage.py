b825da36bdfdbc9e4bcf0c41620e855186947f5c
mathg15 <mathis.granger15@gmail.com>
2021-09-01 19:58:15 +0200

    Update homepage.py

--- Pages/homepage.py
+++ Pages/homepage.py
@@ -6,4 +6,7 @@ def homepage():
     st.write("")
     st.write("Le logiciel Moosh a pour objectif de modéliser un faisceau lumineux à travers un structure multicouche.")
     st.header("Examples de réalisations : ")
-    st.image
+    st.markdown("## Coefficient de réflexion en fonction de l'angle pour une structure multicouche contenant du Chrome")
+    st.image("./Images/reflexionCrTM.png")
+    st.markdown("## Faisceau lumineux dans un lame de verre SiO2")
+    st.image("./Images/beam")
