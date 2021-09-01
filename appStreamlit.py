42226e522d57aa2f60f34fc8cde46a956712fcfe
mathg15 <69726961+mathg15@users.noreply.github.com>
2021-09-01 15:23:07 +0200

    Update appStreamlit.py

--- appStreamlit.py
+++ appStreamlit.py
@@ -23,6 +23,31 @@ st.set_page_config(page_title="Moosh", page_icon="./Images/logo_moosh.jpg")
 
 widget = sidebarWidget()
 
+langSelec = st.sidebar.selectbox("", ("France", "English"))
+
+if langSelec == 'France':
+
+    from Pages.homepage import *
+    from Pages.MooshGenPage import *
+    from Pages.ExemplesPage import *
+    from Pages.docs import *
+    from Pages.maj import maj
+
+    ex = "Exemples"
+    up = "Mise à jour"
+
+elif langSelec == 'English':
+
+    from PagesEN.homepage import *
+    from PagesEN.MooshGenPage import *
+    from PagesEN.ExemplesPage import *
+    from PagesEN.docs import *
+    from PagesEN.maj import maj
+
+    ex = "Examples"
+    up = "Update"
+
+
 st.sidebar.image("./Images/logo_moosh.jpg")
 st.sidebar.title("Navigation")
 st.sidebar.write('')
