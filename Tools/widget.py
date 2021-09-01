2a760fa2f96012eaa12e7f2a96d0e409c83f54fd
mathg15 <69726961+mathg15@users.noreply.github.com>
2021-09-01 16:20:38 +0200

    Update widget.py

--- Tools/widget.py
+++ Tools/widget.py
@@ -4,19 +4,19 @@ import streamlit as st
 class sidebarWidget:
 
     def sliderPara(self):
-        return st.slider("Nombre de miroirs", 1, 50, 20, 1)
+        return st.slider("Nombre de miroirs / Number of mirrors", 1, 50, 20, 1)
 
     def checkPara(self):
-        return st.radio("Polarisation", ("Polarisation TE", "Polarisation TM"))
+        return st.radio("Polarisation", ("TE", "TM"))
 
     def angleInput1(self):
-        return st.number_input("Angle d'incidence", 1, 90, 35, format=None, key=f"{1}")
+        return st.number_input("Angle d'incidence / Incidence angle", 1, 90, 35, format=None, key=f"{1}")
 
     def angleInput2(self):
-        return st.number_input("Angle d'incidence", 0, 90, 35, format=None, key=f"{2}")
+        return st.number_input("Angle d'incidence / Incidence angle", 0, 90, 35, format=None, key=f"{2}")
 
     def angleInput3(self):
-        return st.number_input("Angle d'incidence", 0, 90, 35, format=None, key=f"{3}")
+        return st.number_input("Angle d'incidence / Incidence angle", 0, 90, 35, format=None, key=f"{3}")
 
     def lambdaInput1(self):
         return st.number_input("Longueur d'onde / Wavelength", 400, 800, 600, format=None, key=f"{21}")
@@ -33,71 +33,68 @@ class sidebarWidget:
     def beamPos(self):
         return st.slider("Position", 0.0, 1.0, 0.4, 0.1)
 
-    def selectBox1(self):
-        return st.selectbox("Choix du verre", ("BK7", "SiO2", "TiO2"))
-
     def struslider(self):
-        return st.slider("Nombres de couches", 1, 10, 1, 1)
+        return st.slider("Nombres de couches / Number of layers", 1, 10, 1, 1)
 
     def selecBoxGen1(self):
-        return st.selectbox("Choix du matériau 1", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"))
+        return st.selectbox("Choix du matériau 1 / Choice of the material 1", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"))
 
     def hauteurGen1(self):
-        return st.number_input("Epaisseur du matériau 1", 1, 20000, 400, 1, key=f"{1}")
+        return st.number_input("Epaisseur du matériau 1 / Thickness of the material 1", 1, 20000, 400, 1, key=f"{1}")
 
     def selecBoxGen2(self):
-        return st.selectbox("Choix du matériau 2", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"))
+        return st.selectbox("Choix du matériau 2 / Choice of the material 2 ", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"))
 
     def hauteurGen2(self):
-        return st.number_input("Epaisseur du matériau 2", 1, 20000, 400, 1,  key=f"{2}")
+        return st.number_input("Epaisseur du matériau 2 / Thickness of the material 2", 1, 20000, 400, 1,  key=f"{2}")
 
     def selecBoxGen3(self):
-        return st.selectbox("Choix du matériau 3", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"))
+        return st.selectbox("Choix du matériau 3 / Choice of the material 3", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"))
 
     def hauteurGen3(self):
-        return st.number_input("Epaisseur du matériau 3", 1, 20000, 400, 1, key=f"{3}")
+        return st.number_input("Epaisseur du matériau 3 / Thickness of the material 3", 1, 20000, 400, 1, key=f"{3}")
 
     def selecBoxGen4(self):
-        return st.selectbox("Choix du matériau 4", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"), key=f"{144}")
+        return st.selectbox("Choix du matériau 4 / Choice of the material 4", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"), key=f"{144}")
 
     def hauteurGen4(self):
-        return st.number_input("Epaisseur du matériau 4", 1, 20000, 400, 1, key=f"{104}")
+        return st.number_input("Epaisseur du matériau 4 / Thickness of the material 4", 1, 20000, 400, 1, key=f"{104}")
 
     def selecBoxGen5(self):
-        return st.selectbox("Choix du matériau 5", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"), key=f"{105}")
+        return st.selectbox("Choix du matériau 5 / Choice of the material 5", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"), key=f"{105}")
 
     def hauteurGen5(self):
-        return st.number_input("Epaisseur du matériau 5", 1, 20000, 400, 1, key=f"{1555}")
+        return st.number_input("Epaisseur du matériau 5 / Thickness of the material 5", 1, 20000, 400, 1, key=f"{1555}")
 
     def selecBoxGen6(self):
-        return st.selectbox("Choix du matériau 6", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"), key=f"{106}")
+        return st.selectbox("Choix du matériau 6 / Choice of the material 6", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"), key=f"{106}")
 
     def hauteurGen6(self):
-        return st.number_input("Epaisseur du matériau 6", 1, 20000, 400, 1, key=f"{166}")
+        return st.number_input("Epaisseur du matériau 6 / Thickness of the material 6", 1, 20000, 400, 1, key=f"{166}")
 
     def selecBoxGen7(self):
-        return st.selectbox("Choix du matériau 7", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"), key=f"{107}")
+        return st.selectbox("Choix du matériau 7 / Choice of the material 7", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"), key=f"{107}")
     
     def hauteurGen7(self):
-        return st.number_input("Epaisseur du matériau 7", 1, 20000, 400, 1, key=f"{177}")
+        return st.number_input("Epaisseur du matériau 7 / Thickness of the material 7", 1, 20000, 400, 1, key=f"{177}")
 
     def selecBoxGen8(self):
-        return st.selectbox("Choix du matériau 8", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"), key=f"{108}")
+        return st.selectbox("Choix du matériau 8 / Choice of the material 8", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"), key=f"{108}")
 
     def hauteurGen8(self):
-        return st.number_input("Epaisseur du matériau 8", 1, 20000, 400, 1, key=f"{188}")
+        return st.number_input("Epaisseur du matériau 8 / Thickness of the material 8", 1, 20000, 400, 1, key=f"{188}")
 
     def selecBoxGen9(self):
-        return st.selectbox("Choix du matériau 9", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"), key=f"{109}")
+        return st.selectbox("Choix du matériau 9 / Choice of the material 9", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"), key=f"{109}")
 
     def hauteurGen9(self):
-        return st.number_input("Epaisseur du matériau 9", 1, 20000, 400, 1, key=f"{199}")
+        return st.number_input("Epaisseur du matériau 9 / Thickness of the material 9", 1, 20000, 400, 1, key=f"{199}")
     
     def selecBoxGen10(self):
-        return st.selectbox("Choix du matériau 10", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"), key=f"{110}")
+        return st.selectbox("Choix du matériau 10 / Choice of the material 10", ("Air", "Eau", "Bk7", "SiO2", "TiO2", "Au", "Cr","Ag","ZnO"), key=f"{110}")
 
     def hauteurGen10(self):
-        return st.number_input("Epaisseur du matériau 10", 1, 20000, 400, 1, key=f"{111}")
+        return st.number_input("Epaisseur du matériau 10 / Thickness of the material 10", 1, 20000, 400, 1, key=f"{111}")
 
     def angleCoefGen(self):
         return st.number_input("Angle d'incidence", 0, 90, 35, format=None, key=f"{4}")
@@ -112,7 +109,7 @@ class sidebarWidget:
         return st.button("Afficher Angular")
 
     def thetaSpecGen(self):
-        return st.number_input("Angle d'incidence", 0, 90, 35, format=None, key=f"{15}")
+        return st.number_input("Angle d'incidence / Incidence angle", 0, 90, 35, format=None, key=f"{15}")
 
     def btnSpecGen(self):
         return st.button("Afficher Spectrum")
@@ -136,7 +133,7 @@ class sidebarWidget:
         return st.number_input("Angle maximum", 0, 90, 90, format=None, key=f"{8}")
 
     def lambBeamGen(self):
-        return st.number_input("Longueur d'onde", 400, 800, 600, key=f"{14}")
+        return st.number_input("Longueur d'onde / Wavelength", 400, 800, 600, key=f"{14}")
 
     def angBeamGen(self):
         return st.number_input("Angle", 1.0, 90.0, 45.0, 0.1, key=f"{9}")
