de11478fd8ffbd6e2c2304966a6a543dcf447cc7
mathg15 <mathis.granger15@gmail.com>
2021-09-01 18:47:37 +0200

    Update docs.py

--- Pages/docs.py
+++ Pages/docs.py
@@ -2,4 +2,11 @@ import streamlit as st
 
 def documentation():
     st.title('Documentation')
+    choix = st.radio("Liste des documents",("Utilisation de l'application","Théorie","Bibliographie"))
+    if choix == "Utilisation de l'application":
+        st.header("Utilisation de l'application")
+    elif choix == "Théorie":
+        st.header("Théorie")
+    elif choix == "Bibliographie"
+        st.header("Bibliographie")
     st.write("Utilisation de Moosh")
